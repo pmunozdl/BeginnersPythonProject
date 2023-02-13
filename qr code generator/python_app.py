b7598531178a74ed42605d6d76d9bf7d4b73bb84
Pablo Muñoz de lorenzo <pablomunozdelorenzo@MacBook-Pro-de-Pablo.local>
2023-02-13 13:33:33 +0100

    qr code generator finished

---
+++ qr code generator/python_app.py
@@ -0,0 +1,21 @@
+#install all the libraries needed
+    #pip install qrcode Image
+#create a function that collects a text and converts it to a qrcode
+#save the qr code as an image
+#run the function
+
+import qrcode
+
+def generate_qrcode(text):
+
+    qr = qrcode.QRCode(
+        version = 1,
+        error_correction = qrcode.constants.ERROR_CORRECT_L,
+        box_size = 10,
+        border = 4,
+    )
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save("qrimg.png")
+generate_qrcode("https://https://www.realracingclub.es/")
